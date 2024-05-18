3bfc2daa98c0eb76021130be074954563c95ceb4
Caroline Claro Corrêa <133122086+carol-claro@users.noreply.github.com>
2024-05-18 20:37:45 -0300

    Update update_config.sh

--- parte_04/update_config.sh
+++ parte_04/update_config.sh
@@ -1,11 +1,11 @@
 
-echo '{                                                     ' >  $(pwd)/part_4/docker/config/microservices.json
-echo '  "model_manager": {                                         ' >> $(pwd)/part_4/docker/config/microservices.json
+echo '{                                                     ' >  $(pwd)/parte_04/docker/config/microservices.json
+echo '  "model_manager": {                                         ' >> $(pwd)/parte_04/docker/config/microservices.json
 echo '    "endpoint": "http://'$(sudo docker inspect modelmanager | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
-['plat_network']['IPAddress'])")':8080"          ' >> $(pwd)/part_4/docker/config/microservices.json
-echo '  }                                                   ' >> $(pwd)/part_4/docker/config/microservices.json
-echo '}                                                     ' >> $(pwd)/part_4/docker/config/microservices.json
+['plat_network']['IPAddress'])")':8080"          ' >> $(pwd)/parte_04/docker/config/microservices.json
+echo '  }                                                   ' >> $(pwd)/parte_04/docker/config/microservices.json
+echo '}                                                     ' >> $(pwd)/parte_04/docker/config/microservices.json
 
-echo "[PART 4] Arquivo de configuração atualizado com sucesso. Veja seu conteúdo: "
+echo "[PARTE 04] Arquivo de configuração atualizado com sucesso. Veja seu conteúdo: "
 
-cat $(pwd)/part_4/docker/config/microservices.json
+cat $(pwd)/parte_04/docker/cossnfig/microservices.json
